51afdb0368bcc963692248d6c85492161272d28a
Ed Snider <edsnider@live.com>
2018-01-10 17:18:08 -0500

    added uwp support

---
+++ src/Plugin.LatestVersion.UWP/LatestVersionImplementation.cs
@@ -0,0 +1,110 @@
+﻿using System;
+using System.Threading.Tasks;
+using Windows.ApplicationModel;
+using Windows.Services.Store;
+using Plugin.LatestVersion.Abstractions;
+
+namespace Plugin.LatestVersion
+{
+    /// <summary>
+    /// <see cref="ILatestVersion"/> implementation for UWP.
+    /// </summary>
+    public class LatestVersionImplementation : ILatestVersion
+    {
+        /// <inheritdoc />
+        public async Task<bool> IsUsingLatestVersion()
+        {
+            try
+            {
+                var context = StoreContext.GetDefault();
+                var updates = await context.GetAppAndOptionalStorePackageUpdatesAsync();
+
+                return updates.Count > 0;
+            }
+            catch (Exception e)
+            {
+                throw new LatestVersionException($"Error checking for updates available in the Store.", e);
+            }
+        }
+
+        /// <inheritdoc />
+        public async Task<string> GetLatestVersionNumber()
+        {
+            try
+            {
+                var version = Package.Current.Id.Version.ToString();
+                var context = StoreContext.GetDefault();
+                var updates = await context.GetAppAndOptionalStorePackageUpdatesAsync();
+                
+                if (updates.Count > 0)
+                {
+                    version = updates[0]?.Package?.Id?.Version.ToString();
+                }
+
+                return version;
+            }
+            catch (Exception e)
+            {
+                throw new LatestVersionException($"Error getting the latest version number available in the Store.", e);
+            }
+        }
+
+        /// <inheritdoc />
+        public async Task<string> GetLatestVersionNumber(string appName)
+        {
+            if (string.IsNullOrWhiteSpace(appName))
+            {
+                throw new ArgumentNullException(nameof(appName));
+            }
+
+            // NOTE: This method is currently not support for UWP, so returning string.Empty
+
+            return await Task.FromResult(string.Empty);
+        }
+
+        /// <inheritdoc />
+        public void OpenAppInStore()
+        {
+            try
+            {
+                Task.Run(async () =>
+                {
+                    var context = StoreContext.GetDefault();
+                    var product = await context.GetStoreProductForCurrentAppAsync();
+                    var storeId = product.Product.StoreId;
+
+                    OpenAppInStore(storeId);
+                });
+            }
+            catch (Exception e)
+            {
+                throw new LatestVersionException($"Unable to open the current app in the Store.", e);
+            }
+        }
+
+        /// <inheritdoc />
+        public void OpenAppInStore(string appName)
+        {
+            if (string.IsNullOrWhiteSpace(appName))
+            {
+                throw new ArgumentNullException(nameof(appName));
+            }
+
+            try
+            {
+                if (Guid.TryParse(appName, out Guid appId))
+                {
+                    Windows.System.Launcher.LaunchUriAsync(new Uri($"ms-windows-store://pdp/?AppId={appName}"));
+                }
+                else
+                {
+                    Windows.System.Launcher.LaunchUriAsync(new Uri($"ms-windows-store://pdp/?ProductId={appName}"));
+                }
+            }
+            catch (Exception e)
+            {
+                throw new LatestVersionException($"Unable to open {appName} in the Store.", e);
+            }
+        }
+    }
+}
